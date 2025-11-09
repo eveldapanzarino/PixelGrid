cbb0fdbf4e10aefa60ec385fbef0a0269b55762a
eveldapanzarino <everose7023@gmail.com>
2025-11-09 10:29:13 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+server: {
+    allowedHosts: [
+      'devserver-main--react-grid-responsive-graphics-render.netlify.app',
+    ],
+  },
 })
+
+export default defineConfig({
+  plugins: [react()],
+  
