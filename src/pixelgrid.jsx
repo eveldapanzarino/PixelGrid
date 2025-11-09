ce8e3f9a8e4b6241027e1861915d6406204778b3
eveldapanzarino <everose7023@gmail.com>
2025-11-09 13:11:38 -0500

    Update pixelgrid.jsx

--- src/pixelgrid.jsx
+++ src/pixelgrid.jsx
@@ -20,8 +20,8 @@ export default function PixelGrid() {
         width: `${size.w}px`,
         height: `${size.h}px`,
         display: "grid",
-        gridTemplateColumns: `repeat(250, 2.5vw`,
-        gridTemplateRows: `repeat(160, 5vh)`,
+        gridTemplateColumns: `repeat(250, 2vw`,
+        gridTemplateRows: `repeat(160, 2vh)`,
 
       }}
     >
