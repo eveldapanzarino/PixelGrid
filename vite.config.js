bbf94a353e8d357fb028b0337c78b9a5f43546ca
eveldapanzarino <everose7023@gmail.com>
2025-11-09 11:20:22 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-server: {
-    allowedHosts: [
-      'devserver-main--react-grid-responsive-graphics-render.netlify.app',
-    ],
+  build: {
+    outDir: 'dist', // this is what Netlify will publish
+  },
+  server: {
+    allowedHosts: ['devserver-main--react-grid-responsive-graphics-render.netlify.app'], // optional fix for your earlier error
   },
 })
