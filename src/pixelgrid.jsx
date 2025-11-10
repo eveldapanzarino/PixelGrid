0290710e0756df7c754df724b4bd60b0c756f24a
eveldapanzarino <everose7023@gmail.com>
2025-11-10 08:58:29 -0500

    Update pixelgrid.jsx

--- src/pixelgrid.jsx
+++ src/pixelgrid.jsx
@@ -19,7 +19,7 @@ export default function PixelGrid() {
   const totalPixels = 250 * size.h;
   const pixels = Array.from({ length: totalPixels });
 
-  const cellVW = size.w / 100;
+  const cellVW = size.w / 100; // px per 1vw
   const rows = Math.floor(size.h / cellVW);
 
   function paintPixel(e) {
@@ -35,7 +35,7 @@ export default function PixelGrid() {
         gridTemplateColumns: `repeat(250, 1vw)`,
         gridTemplateRows: `repeat(${rows}, 1vw)`,
         userSelect: "none",
-        touchAction: "none",
+        touchAction: "none", // IMPORTANT for mobile drawing
       }}
     >
       {pixels.map((_, i) => (
@@ -44,7 +44,7 @@ export default function PixelGrid() {
           id={`pixel-${i}`}
           className="pixelgrid"
           style={{
-            background: "white"
+            background: "white",
           }}
           onPointerDown={(e) => {
             setIsDrawing(true);
