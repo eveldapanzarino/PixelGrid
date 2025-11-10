0f7f3f334817c3378417c9aaf089943a32dabe10
eveldapanzarino <everose7023@gmail.com>
2025-11-10 10:39:24 -0500

    Update pixelgrid.jsx

--- src/pixelgrid.jsx
+++ src/pixelgrid.jsx
@@ -20,7 +20,7 @@ export default function PixelGrid() {
   const cellVW = size.w / 100; // px per 1vw
   const rows = Math.floor(size.h / cellVW);
 
-const totalPixels = 250000;
+const totalPixels = 25000;
   const pixels = Array.from({ length: totalPixels });
 
   function paintPixel(e) {
