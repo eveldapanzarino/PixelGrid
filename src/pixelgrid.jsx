820e7d3f38e4db08ac601fff04ff589ee4e8efd9
eveldapanzarino <everose7023@gmail.com>
2025-11-10 10:38:40 -0500

    Update pixelgrid.jsx

--- src/pixelgrid.jsx
+++ src/pixelgrid.jsx
@@ -34,7 +34,7 @@ const totalPixels = 250 * 500;
         height: "100vh",
         display: "grid",
         gridTemplateColumns: `repeat(250, 1vw)`,
-        gridTemplateRows: `repeat(${size.h}, 1vw)`,
+        gridTemplateRows: `repeat(${rows}, 1vw)`,
         userSelect: "none",
         touchAction: "none", // IMPORTANT for mobile drawing
       }}
