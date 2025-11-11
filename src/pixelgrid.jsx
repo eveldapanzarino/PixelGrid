298d72677f9cd01b4220c4bdbf42586842a3b70b
Eve Panzarino <everose7023@gmail.com>
2025-11-10 21:37:14 -0500

    Update pixelgrid.jsx

--- src/pixelgrid.jsx
+++ src/pixelgrid.jsx
@@ -28,14 +28,14 @@ const totalPixels = 250 * rows * 1.2;
   }
 
   return (
-    <div style=
+    <div style={{
       display: "grid", 
       width: "8vw",
         height: "100vh", 
       background-color: "black", 
       gridTemplateColumns: `repeat(1, 8vw)`,
-        gridTemplateRows: `repeat(5, 8vw) } 
-        class="color-palette">
+        gridTemplateRows: `repeat(5, 8vw) }} 
+        >
        <div class="color-swatch"></div>
        <div class="color-swatch"></div>
        <div class="color-swatch"></div>
