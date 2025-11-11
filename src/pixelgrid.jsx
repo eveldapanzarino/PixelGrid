167ead7d2db13b721f612313551d96dd837e6f95
Eve Panzarino <everose7023@gmail.com>
2025-11-11 00:47:17 -0500

    Update pixelgrid.jsx

--- src/pixelgrid.jsx
+++ src/pixelgrid.jsx
@@ -148,12 +148,12 @@ export default function PixelGrid() {
       window.removeEventListener("pointerup", () => setIsDrawing(false));
     };
   }, []);
-
-  const totalPixels = 250 * 160;
+const rows = Math.floor(size.h / cellVW);
+  const totalPixels = 250 * rows;
   const pixels = Array.from({ length: totalPixels });
 
   const cellVW = size.w / 100; // px per 1vw
-  const rows = Math.floor(size.h / cellVW);
+  
 
   function paintPixel(e) {
     e.target.style.background = "blue";
@@ -165,7 +165,7 @@ export default function PixelGrid() {
         width: "100vw",
         height: "100vh",
         display: "grid",
-        gridTemplateColumns: `repeat(100, 1vw)`,
+        gridTemplateColumns: `repeat(250, 1vw)`,
         gridTemplateRows: `repeat(${rows}, 1vw)`,
         userSelect: "none",
         touchAction: "none", // IMPORTANT for mobile drawing
