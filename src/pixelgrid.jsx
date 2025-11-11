00b1b3be779b89077287cc69e1a8f42d21ca5110
eveldapanzarino <everose7023@gmail.com>
2025-11-11 23:48:40 +0000

    .

--- src/pixelgrid.jsx
+++ src/pixelgrid.jsx
@@ -268,7 +268,7 @@ const [showFileMenu, setShowFileMenu] = useState(false);
     }}
     maxLength={7}
     style={{
-      width: "9vh",
+      width: "5vh",
       marginTop: "1vw",
       background: "#111",
       border: "0.3vw solid #666",
