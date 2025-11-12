5eccdeba078bdb46367c046ecfa847d5c11fab91
eveldapanzarino <everose7023@gmail.com>
2025-11-12 02:05:28 +0000

    .

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true, // listens on 0.0.0.0
-    port: 8080, // match DigitalOcean health check
+    port: 5173, // match DigitalOcean health check
     allowedHosts: [
       'pixelgrid-furb8.ondigitalocean.app',
       'localhost'
