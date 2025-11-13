c2e1b6254bb27513e53b076665c8d17b03374ded
Eve Panzarino <everose7023@gmail.com>
2025-11-13 17:12:27 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
   build: {
     outDir: 'dist'
   }
+})
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/pixel-grid/', // Replace 'pixel-grid' with your repository name
 })
